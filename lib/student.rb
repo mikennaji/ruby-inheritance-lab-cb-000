c21f3d3b26550bd4d7161ae013e2deee394bdd4f
Michael Nnaji <mnnaji3@uwo.ca>
2020-05-02 02:39:02 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student  < User
+
+require_relative "./user.rb"
+
+attr_accessor :first_name, :last_name, :knowledge
+
+def initialize
+  @first_name = first_name
+  @last_name = last_name
+  @knowledge = []
+end
+
+def learn(args)
+  @knowledge << args
+end
+
+
 
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,20 @@
-class Teacher
-
+class Teacher < User
+require_relative "./user.rb"
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+attr_accessor :first_name, :last_name
+
+def initialize
+  @first_name =first_name
+  @last_name =last_name
+end
+
+
+def teach
+  KNOWLEDGE[rand(1...KNOWLEDGE.size)]
+end
+
+
+
+
 end
